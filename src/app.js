56fec84610cbfd71e06e26a41603953dbdefd244
ajitpawarink <ajpawar321@gmail.com>
2025-12-20 10:57:47 +0530

    adding middlewares

--- src/app.js
+++ src/app.js
@@ -1,29 +1,35 @@
 const express = require('express');
 const app = express();
+const { adminAuth,userAuth  } = require('./middlewares/auth');
+
+
+
+app.use('/user', userAuth);
+app.use('/admin', adminAuth);
+
 
 app.get('/user/:userid', (req,resp,next)=>{
     console.log(req.query)
     console.log(req.params)
-    next();
-},(req,resp)=>{
-    console.log("second callback")
     resp.send("get User");
 });
 
 app.post('/user', (req,resp)=>{
+    console.log(req.query)
+    console.log(req.params)
     resp.send("post User")
 });
 
-app.use('/test', (req,resp)=>{
-    resp.send("heelo test")
+app.get('/admin', (req,resp,next)=>{
+    console.log(req.query)
+    console.log(req.params)
+    resp.send("get User");
 });
 
-app.use('/hello', (req,resp)=>{
-    resp.send("heelo world")
-});
-
-app.use((req,resp)=>{
-    resp.send("heelo dash")
+app.post('/admin', (req,resp)=>{
+    console.log(req.query)
+    console.log(req.params)
+    resp.send("post User")
 });
 
 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,21 @@
+const adminAuth = (req,resp, next)=>{
+    console.log('running admin auth middleware');
+    const token = 'xyzz';
+    if(token === 'xyz'){
+        next();
+    }else{
+        resp.status(403).send("Forbidden Not authorized to Access Admin Resource");
+    }
+}
+
+const userAuth = (req,resp, next)=>{
+    console.log('running user auth middleware');
+    const token = 'xyz';
+    if(token === 'xyz'){
+        next();
+    }else{
+        resp.status(403).send("Forbidden Not authorized to Access user Resource");
+    }
+}
+
+ module.exports = { adminAuth, userAuth };
